685da8ed604e02c69aad9aa575c17022fa129659
Marto <martinkaldramov1@gmail.com>
2017-09-09 22:33:59 +0300

    created a couple more routes that return some json data

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
- res.send('<h1>Testing if express app is working</h1>'); 
+ res.send({
+   name: 'Some Name',
+   likes: [
+     'Biking',
+     'Cities'
+   ]
+ }); 
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page'); 
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad Request'  
+  }); 
 });
 
 app.listen(3000);
