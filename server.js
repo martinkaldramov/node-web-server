505bf5bc8c63d2eec52f085133bfa6fb2e4550d8
Marto <martinkaldramov1@gmail.com>
2017-09-11 14:45:41 +0300

    using Express Middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -7,8 +8,28 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+  var timeStamp = new Date().toString();
+  var log = `${timeStamp}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err)
+      console.log('Error writing to server.log');
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintanance.hbs');  
+});
+
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear(); 
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
